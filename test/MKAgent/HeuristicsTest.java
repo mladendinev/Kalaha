550b97fd20d1a8481626b634971af660b11f1025
haralav2 <venih@mail.bg>
2015-12-06 20:57:05 +0000

    Implemented the heuristics and added tests to verify thier correctness

---
+++ test/MKAgent/HeuristicsTest.java
@@ -0,0 +1,17 @@
+package MKAgent;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by mbax2vh2 on 06/12/15.
+ */
+public class HeuristicsTest extends TestCase {
+
+    public void testCanGetExtraTurn() throws Exception {
+
+    }
+
+    public void testHoleCapture() throws Exception {
+
+    }
+}
