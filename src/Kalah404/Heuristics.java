6e2385e57ec655809f7e689bbcddfb117fdfeaf3
haralav2 <venih@mail.bg>
2015-12-12 17:54:27 +0000

    Removing old files

--- src/Kalah404/Heuristics.java
+++ src/Kalah404/Heuristics.java
@@ -12,38 +12,6 @@ public class Heuristics {
 
     private static final int KALAHA_LOCATION = 8;
 
-    public static int monteCarlo(Node node, int timesRun){
-
-        int average = 0;
-
-        for(int i = 0; i < timesRun; i++){
-            average += monteCarlo(node);
-        }
-
-        average /= timesRun;
-
-        return average;
-    }
-
-    public static int monteCarlo(Node node){
-
-        Board board = new Board(node.getBoard());
-        Side side = node.getSide();
-
-        while(!Kalah.gameOver(board)){
-
-            List<Integer> validHoles = board.getValidHoles(side);
-
-            int randomHole = validHoles.get(random.nextInt(validHoles.size()));
-
-            Move move = new Move(side, randomHole);
-
-            side = Kalah.makeMove(board, move);
-        }
-
-        return board.getSeedsInStore(Side.mySide) - board.getSeedsInStore(Side.mySide.opposite()); //todo have this outside
-    }
-
     public static int getScore(Node node) {
 
         Board board = node.getBoard();
